9bb5610a70ef503a1d182d2382ceec5c1d538d0b
Harshit Negi <harshitnegi226@gmail.com>
2025-11-11 17:45:38 +0530

    database

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require("sequelize");
-require('dotenv').config()
+require("dotenv").config();
 
-const sequelize= new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
-    host:process.env.DB_HOST,
-    dialect:'mysql'
-})
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+  logging: false,
+});
 
-module.exports=sequelize
+module.exports = sequelize;
